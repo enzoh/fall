cfb47608104f3d3f92cb70074fa603dee13afd46
enzoh <enzo@sovereign.io>
2018-02-16 12:34:48 -0800

    Move source references into source module

--- Language/WebAssembly/AST.hs
+++ Language/WebAssembly/AST.hs
@@ -1,56 +1,12 @@
 module Language.WebAssembly.AST where
 
-import Data.Default.Class (Default(..))
-import Data.Int           (Int32)
-import Data.Maybe         (fromJust, isNothing)
-import Data.Word          (Word32)
-import Text.Printf        (printf)
+import Data.Int   (Int32)
+import Data.Maybe (fromJust, isNothing)
+import Data.Word  (Word32)
 
-import Language.WebAssembly.Operator 
+import Language.WebAssembly.Operator
+import Language.WebAssembly.Source
 import Language.WebAssembly.Type
-
-data Position
-   = Position
-   { _posFile   :: FilePath
-   , _posLine   :: Int
-   , _posColumn :: Int
-   } deriving (Eq)
-
-instance Default Position where
-   def = Position "" 0 0
-
-instance Show Position where
-   show Position {..} =
-      if _posLine == -1
-      then printf "0x%x" _posColumn
-      else printf "%d.%d" _posLine $ succ _posColumn
-
-data Region
-   = Region
-   { _regionLeft  :: Position
-   , _regionRight :: Position
-   } deriving (Eq)
-
-instance Default Region where
-   def = Region def def
-
-instance Show Region where
-   show Region {..} =
-      printf "%s:%s" file area
-      where file = _posFile _regionLeft
-            area = show _regionLeft ++
-               if _regionLeft == _regionRight
-               then "" else "-" ++ show _regionRight
-
-data Phrase a
-   = Phrase
-   { _phraseAt :: Region
-   , _phraseIt :: a
-   } deriving (Eq, Show)
-
-infixl 5 @@
-(@@) :: a -> Region -> Phrase a
-(@@) = flip Phrase
 
 matchLimit :: Limit Word32 -> Limit Word32 -> Bool
 matchLimit lim1 lim2 =
---
+++ Language/WebAssembly/Source.hs
@@ -0,0 +1,47 @@
+module Language.WebAssembly.Source where
+
+import Data.Default.Class (Default(..))
+import Text.Printf        (printf)
+
+data Position
+   = Position
+   { _posFile   :: FilePath
+   , _posLine   :: Int
+   , _posColumn :: Int
+   } deriving (Eq)
+
+instance Default Position where
+   def = Position "" 0 0
+
+instance Show Position where
+   show Position {..} =
+      if _posLine == -1
+      then printf "0x%x" _posColumn
+      else printf "%d.%d" _posLine $ succ _posColumn
+
+data Region
+   = Region
+   { _regionLeft  :: Position
+   , _regionRight :: Position
+   } deriving (Eq)
+
+instance Default Region where
+   def = Region def def
+
+instance Show Region where
+   show Region {..} =
+      printf "%s:%s" file area
+      where file = _posFile _regionLeft
+            area = show _regionLeft ++
+               if _regionLeft == _regionRight
+               then "" else "-" ++ show _regionRight
+
+data Phrase a
+   = Phrase
+   { _phraseAt :: Region
+   , _phraseIt :: a
+   } deriving (Eq, Show)
+
+infixl 5 @@
+(@@) :: a -> Region -> Phrase a
+(@@) = flip Phrase
