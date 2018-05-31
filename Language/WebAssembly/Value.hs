9c9b8361daa821f7ba99643b8d3a9d0c56fab278
Enzo Haussecker <enzo@sovereign.io>
2018-05-31 08:56:21 -0700

    Implement value type

--- Language/WebAssembly/Value.hs
+++ Language/WebAssembly/Value.hs
@@ -1,5 +1,30 @@
 module Language.WebAssembly.Value where
 
+import Data.Bool (bool)
+import Data.Word (Word32, Word64)
+
+import Language.WebAssembly.Type
+
 data Value
-   = Value
+   = I32 Word32
+   | I64 Word64
+   | F32 Float
+   | F64 Double
    deriving (Show)
+
+typeOf :: Value -> ValueType
+typeOf = \ case
+   I32 _ -> I32Type
+   I64 _ -> I64Type
+   F32 _ -> F32Type
+   F64 _ -> F64Type
+
+defaultValue :: ValueType -> Value
+defaultValue = \ case
+   I32Type -> I32 0
+   I64Type -> I64 0
+   F32Type -> F32 0
+   F64Type -> F64 0
+
+valueOfBool :: Bool -> Value
+valueOfBool = I32 . bool 0 1
